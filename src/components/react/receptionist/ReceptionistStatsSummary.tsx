86a539c2c3ef7c5f759a467d1a10b47524f2e23d
Heraclesenmanuel <sanchezheracles@gmail.com>
2026-03-15 21:55:40 +0100

    Bugfix de botones en botones tras responsive de - Agenda Maestra Receptionist

--- src/components/react/receptionist/ReceptionistStatsSummary.tsx
+++ src/components/react/receptionist/ReceptionistStatsSummary.tsx
@@ -16,7 +16,7 @@ interface ReceptionistStatsSummaryProps {
 
 export const ReceptionistStatsSummary = ({ stats }: ReceptionistStatsSummaryProps) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="px-4 md:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             {stats.map((stat, i) => (
                 <StatsCard
                     key={i}
